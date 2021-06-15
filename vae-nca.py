vae-nca, overfit, alive 1, 0 kl, learned p, fixed z, lr=1e-4

vae-nca.py
@@ -85,7 +85,7 @@ class Model(nn.Module):
             print(n, p.shape)
 
         self.to(self.device)
-        self.optimizer = optim.Adam(self.parameters(), lr=1e-3)
+        self.optimizer = optim.Adam(self.parameters(), lr=1e-4)
         self.batch_idx = 0
 
     def train_batch(self):