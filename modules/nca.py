vae-nca, overfit, alive 1, 0 kl, learned p, fixed z, 1 p_update, 1e-4 lr, alive

modules/nca.py
@@ -31,7 +31,7 @@ class MitosisNCA(t.nn.Module):
 
         post_alive = self.alive_mask(state)
         alive_mask = pre_alive * post_alive
-        return state * alive_mask
+        return state  # * alive_mask
 
     def forward(self, state):
         state.sg("bchw")