8da0036973d56deb8c4177eaea6e8d06b21685bb
rasmusbergpalm <rasmusbergpalm@gmail.com>
2021-09-10 08:46:26 +0200

    noto emoji z=128, hid=128, bs=4

--- vae-nca.py
+++ vae-nca.py
@@ -52,9 +52,10 @@ class VAENCA(Model, nn.Module):
         self.train_samples = 1
         self.test_loss_fn = self.iwae_loss_fn
         self.test_samples = 1
-        self.hidden_size = 256
+        self.hidden_size = 128
 
         batch_size = 4
+
 
         self.device = "cuda" if torch.cuda.is_available() else "cpu"
 
