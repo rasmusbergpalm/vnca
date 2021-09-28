cf2bce0535fdd18c79368e003848aa23509d1c74
rasmusbergpalm <rasmusbergpalm@gmail.com>
2021-09-28 10:36:14 +0200

    pixelcnn dml, celeba, 256, n_mix = 4, parallel

--- vae-nca.py
+++ vae-nca.py
@@ -23,6 +23,9 @@ from noto import NotoEmoji
 from train import train
 from util import get_writers
 
+t.Tensor.sg = lambda x: x
+t.distributions.Distribution.sg = lambda x: x
+
 
 # torch.autograd.set_detect_anomaly(True)
 
