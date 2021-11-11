be8e32464d687477906bc733d3c5664a37466a0d
rasmusbergpalm <rasmusbergpalm@gmail.com>
2021-11-11 15:58:59 +0100

    celeba, bs=32, 4 layer decoder, dml sample mean

--- modules/dml.py
+++ modules/dml.py
@@ -23,7 +23,7 @@ class DiscretizedMixtureLogitsDistribution(Distribution):
         """
         Returns the mean of the distribution.
         """
-        return self.sample()  # TODO
+        return t.stack([self.sample() for _ in range(1000)], dim=0).mean(dim=0)
 
 
 class DiscretizedMixtureLogits():
