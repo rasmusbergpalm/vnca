44e540540795bf71b4994dfe2917289a94d0f00c
Miguel González Duque <miguelgondu@gmail.com>
2021-11-15 16:38:46 +0100

    Implements a deconv decoder, finally getting it right

--- baseline.py
+++ baseline.py
@@ -3,9 +3,11 @@ from typing import Tuple
 import imageio
 import numpy as np
 import torch as t
+import torch.nn as nn
 from torch import optim
 from torch.distributions import Normal, Bernoulli, kl_divergence
 from torch.utils.data import TensorDataset, DataLoader
+from shapeguard import ShapeGuard
 
 from modules.iterable_dataset_wrapper import IterableWrapper
 from modules.model import Model
@@ -26,16 +28,102 @@ class VAE(Model):
         self.device = "cuda" if t.cuda.is_available() else "cpu"
         self.z_dim = self.z_size = z_dim
 
-        self.encoder = t.nn.Sequential(
-            t.nn.Linear(28 * 28, 512),
-            t.nn.ReLU(),
-            t.nn.Linear(512, 2 * z_dim),
+        filter_size = 5
+        pad = filter_size // 2
+        encoder_hid = 32
+        h = w = 28
+        n_channels = 1
+        self.h = h
+        self.w = w
+        self.encoder_hid = encoder_hid
+        self.n_channels = n_channels
+
+        self.encoder = nn.Sequential(
+            nn.Conv2d(n_channels, encoder_hid * 2 ** 0, filter_size, padding=pad + 2),
+            nn.ELU(),  # (bs, 32, h, w)
+            nn.Conv2d(
+                encoder_hid * 2 ** 0,
+                encoder_hid * 2 ** 1,
+                filter_size,
+                padding=pad,
+                stride=2,
+            ),
+            nn.ELU(),  # (bs, 64, h//2, w//2)
+            nn.Conv2d(
+                encoder_hid * 2 ** 1,
+                encoder_hid * 2 ** 2,
+                filter_size,
+                padding=pad,
+                stride=2,
+            ),
+            nn.ELU(),  # (bs, 128, h//4, w//4)
+            nn.Conv2d(
+                encoder_hid * 2 ** 2,
+                encoder_hid * 2 ** 3,
+                filter_size,
+                padding=pad,
+                stride=2,
+            ),
+            nn.ELU(),  # (bs, 256, h//8, w//8)
+            nn.Conv2d(
+                encoder_hid * 2 ** 3,
+                encoder_hid * 2 ** 4,
+                filter_size,
+                padding=pad,
+                stride=2,
+            ),
+            nn.ELU(),  # (bs, 512, h//16, w//16),
+            nn.Flatten(),  # (bs, 512*h//16*w//16)
+            nn.Linear(encoder_hid * (2 ** 4) * 2 * 2, 2 * self.z_size),
         )
 
+        self.decoder_linear = nn.Sequential(
+            nn.Linear(self.z_size, encoder_hid * (2 ** 4) * 2 * 2)
+        )
         self.decoder = t.nn.Sequential(
-            t.nn.Linear(z_dim, 512),
-            t.nn.ReLU(),
-            t.nn.Linear(512, 28 * 28),
+            t.nn.ConvTranspose2d(
+                encoder_hid * 2 ** 4,  # (bs, 512, h//16, w//16)
+                encoder_hid * 2 ** 3,
+                filter_size,
+                padding=pad,
+                output_padding=1,
+                stride=2,
+            ),
+            t.nn.ELU(),
+            nn.ConvTranspose2d(
+                encoder_hid * 2 ** 3,
+                encoder_hid * 2 ** 2,
+                filter_size,
+                padding=pad,
+                output_padding=1,
+                stride=2,
+            ),
+            t.nn.ELU(),
+            nn.ConvTranspose2d(
+                encoder_hid * 2 ** 2,
+                encoder_hid * 2 ** 1,
+                filter_size,
+                padding=pad,
+                output_padding=1,
+                stride=2,
+            ),
+            t.nn.ELU(),
+            nn.ConvTranspose2d(
+                encoder_hid * 2 ** 1,
+                encoder_hid * 2 ** 0,
+                filter_size,
+                padding=pad,
+                output_padding=1,
+                stride=2,
+            ),
+            t.nn.ELU(),
+            nn.ConvTranspose2d(
+                encoder_hid * 2 ** 0,
+                n_channels,
+                filter_size,
+                padding=pad + 2,
+                # output_padding=1,
+            ),
         )
 
         data, labels = get_binarized_MNIST_with_labels()
@@ -59,22 +147,28 @@ class VAE(Model):
         )
 
         self.p_z = Normal(loc=t.zeros((z_dim)), scale=t.ones((z_dim)))
+        self.to(self.device)
         self.optimizer = optim.Adam(self.parameters(), lr=1e-4)
         self.batch_idx = 0
 
     def encode(self, x: t.Tensor) -> Normal:
-        mu_and_logsigma = self.encoder(x.flatten(start_dim=1))
+        mu_and_logsigma = self.encoder(x)
         mu = mu_and_logsigma[:, : self.z_dim]
         logsigma = mu_and_logsigma[:, self.z_dim :]
 
         return Normal(loc=mu, scale=t.exp(0.5 * logsigma))
 
     def decode(self, z: t.Tensor) -> Bernoulli:
-        logits = self.decoder(z)
+        b, _ = z.shape
+        res = self.decoder_linear(z).view(b, self.encoder_hid * (2 ** 4), 2, 2)
+        logits = self.decoder(res)
 
         return Bernoulli(logits=logits.view(-1, 28, 28))
 
     def forward(self, x: t.Tensor):
+        ShapeGuard.reset()
+        x.sg("bhw")
+        x = x.unsqueeze(1).sg("bchw")
         q_z_given_x = self.encode(x)
 
         z = q_z_given_x.rsample()
