45037c34e4d19388e83fe19c6f7e10930d3d8e2d
Miguel González Duque <miguelgondu@gmail.com>
2021-11-19 13:30:57 +0100

    Adds saving and loading

--- baseline.py
+++ baseline.py
@@ -297,6 +297,22 @@ class VAE(Model, nn.Module):
             # Damage
             # How do I visualize this?
 
+    def save(self, fn):
+        t.save(
+            {
+                "batch_idx": self.batch_idx,
+                "model_state_dict": self.state_dict(),
+                "optimizer_state_dict": self.optimizer.state_dict(),
+            },
+            fn,
+        )
+
+    def load(self, fn):
+        checkpoint = t.load(fn, map_location=t.device(self.device))
+        self.batch_idx = checkpoint["batch_idx"]
+        self.load_state_dict(checkpoint["model_state_dict"])
+        self.optimizer.load_state_dict(checkpoint["optimizer_state_dict"])
+
 
 if __name__ == "__main__":
     vae = VAE()
